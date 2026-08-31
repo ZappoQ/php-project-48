0ce3862d5c3b9807f4db901035cf522af50e2931
Дом <dom@MacBook-Air-Dom.local>
2026-09-01 01:45:13 +0500

    added support for both types

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -4,12 +4,19 @@ namespace Differ\Differ;
 
 function genDiff($data1, $data2, string $format = 'stylish'): string
 {
-    // Если переданы строки - парсим их как JSON
     if (is_string($data1)) {
         $data1 = json_decode($data1, true);
     }
     if (is_string($data2)) {
         $data2 = json_decode($data2, true);
+    }
+
+    // Если всё ещё не массивы - преобразуем
+    if (!is_array($data1)) {
+        $data1 = [];
+    }
+    if (!is_array($data2)) {
+        $data2 = [];
     }
 
     $ast = buildTree($data1, $data2);
