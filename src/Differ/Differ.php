6ea29384dacd0f50e7f18c1c81c079770eca148d
Дом <dom@MacBook-Air-Dom.local>
2026-09-01 01:41:44 +0500

    Fix: accept strings in genDiff for Hexlet tests

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -2,8 +2,16 @@
 
 namespace Differ\Differ;
 
-function genDiff(array $data1, array $data2, string $format = 'stylish'): string
+function genDiff($data1, $data2, string $format = 'stylish'): string
 {
+    // Если переданы строки - парсим их как JSON
+    if (is_string($data1)) {
+        $data1 = json_decode($data1, true);
+    }
+    if (is_string($data2)) {
+        $data2 = json_decode($data2, true);
+    }
+
     $ast = buildTree($data1, $data2);
     $formatter = getFormatter($format);
     return $formatter($ast);
