cd5087165822cb87a9b6b62972cce80664b38860
Дом <dom@MacBook-Air-Dom.local>
2026-09-05 16:28:06 +0500

    FIX: correct closing braces in output

--- src/Differ/Formatters.php
+++ src/Differ/Formatters.php
@@ -7,8 +7,7 @@ function getFormatter(string $format): callable
     switch ($format) {
         case 'stylish':
             return function ($ast) {
-                $result = Formatters\stylish($ast);
-                return "{\n" . $result . "\n}";
+                return Formatters\stylish($ast);
             };
         case 'plain':
             return function ($ast) {
